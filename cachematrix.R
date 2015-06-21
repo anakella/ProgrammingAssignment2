34784beb4cc063acc5cdfa8fdd825af4d578ea26
anakella <anakella@hotmail.com>
2015-06-21 00:43:49 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,25 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The Purpose of this project is to write function to cache expensive computing operation of 
+## calculating inverse of an matrix using the concepts of learned under Lexical Scoping
 
-## Write a short comment describing this function
+## function1: makeCacheMatrix
+## Creates a "Special" Matrix which is a list containing functions
+## 1.set value of a Matrix
+## 2.get value of a matrix
+## 3.set value of inverse
+## 4.get value of inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m<-NULL
+  set<-function(y){
+  x<<-y
+  m<<-NULL
+}
+get<-function() x
+setmatrix<-function(solve) m<<- solve
+getmatrix<-function() m
+list(set=set, get=get,
+   setmatrix=setmatrix,
+   getmatrix=getmatrix)
 }
 
 
